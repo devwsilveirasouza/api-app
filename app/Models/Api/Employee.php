820f13616be815f8d80a0a15cbff00b6bf1c8e3d
devwsilveirasouza <dev.wsilveirasouza@gmail.com>
2022-10-01 08:54:27 -0300

    Api first commit

---
+++ app/Models/Api/Employee.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Api;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['name', 'email', 'salary'];
+}
